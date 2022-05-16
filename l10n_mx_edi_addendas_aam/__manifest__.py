7cbf995e255101f7d2ec5fd396d89ce17c417f76
Istanolion <diego.rojas@digicellmx.com>
2022-05-16 08:46:21 -0500

    Addendas Amazon completed

---
+++ l10n_mx_edi_addendas_aam/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Mexican Addendas Amazon',
+    'author': 'DGL',
+    'license': 'LGPL-3',
+    'category': '',
+    "version": "14.0",
+    'depends': [
+        'base_automation',
+        'l10n_mx_edi_extended',
+        'l10n_mx_edi_addendas_base',
+        
+    ],
+    'test': [],
+    'data': [   
+        'data/addenda_aam.xml'
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
