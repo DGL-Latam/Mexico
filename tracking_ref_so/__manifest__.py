9a24d5c8223a4018ed086c4610a4442a45c5e50a
Istanolion <diego.rojas@digicellmx.com>
2022-06-15 16:00:57 -0500

    the user can now search by tracking Reference in Odoo (SO)

---
+++ tracking_ref_so/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Simple tracking reference search',
+    'version': '15.0',
+    "author": "DGL-Latam",
+    'category': 'Hidden',
+    'summary': 'Modulo para anexar la guia de envio y poderla buscar en las ordenes de venta',
+    'depends': [
+        'sale_management',
+    ],
+    'data': [
+        'views/sale_views.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
