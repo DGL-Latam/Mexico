bed0f9d7aeb065291dd284a4766619b0ee4868c6
Istanolion <diego.rojas@digicellmx.com>
2022-06-16 09:25:01 -0500

    the user can now choose a journal from the sale order that will be reflected on the invoice

---
+++ so_choose_journal/models/sale_order.py
@@ -0,0 +1,21 @@
+from odoo import api,fields,models
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def _get_default_journal(self):
+       return self.env['account.move'].with_context(default_move_type='out_invoice')._get_default_journal()
+
+    journal_id = fields.Many2one(
+        'account.journal', 
+        string='Journal', 
+        required=True,
+        default=_get_default_journal
+    )
+
+    def _prepare_invoice(self):
+        invoice_vals = super()._prepare_invoice()
+        invoice_vals['journal_id'] = self.journal_id.id
+        return invoice_vals
+
+    
