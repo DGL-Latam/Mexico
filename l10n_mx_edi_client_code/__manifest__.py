3fc292a81c953f312c2c3c1f7eeb3aa059f7c4af
Istanolion <diego.rojas@digicellmx.com>
2022-06-23 16:51:18 -0500

    En la factura sale el codigo provisto por el cliente como numero de identificacion en lugar del codigo odoo

---
+++ l10n_mx_edi_client_code/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Client code in edi document MX',
+    'author': 'DGL-Latam',
+    'license': 'LGPL-3',
+    'category': '',
+    "version": "15.0",
+    'depends': [
+        'l10n_mx_edi',        
+    ],
+    'test': [],
+    'data': [   
+        'data/cfdi3_3.xml'
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
