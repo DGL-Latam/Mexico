b4130ee198a332c695261374aea8bc78e5027ac3
Istanolion <diego.rojas@digicellmx.com>
2022-06-28 11:03:13 -0500

    show full cfdi use in invoices

---
+++ full_cfdi_use/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "full cfdi use",
+    "summary": """
+        Muestra tanto el codigo como el nombre del uso del cfdi
+    """,
+    "version": "15.0",
+    "author": "DGL-Latam",
+    "category": "Localization/Mexico",
+    "depends": [
+        'l10n_mx_edi'
+    ],
+    "demo": [
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": False,
+}
