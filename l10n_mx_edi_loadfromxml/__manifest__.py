057d70b633c776879ccf09a88c9cfa155e0ccf15
Istanolion <diego.rojas@digicellmx.com>
2022-06-30 08:12:02 -0500

    basic import from xml edi document

---
+++ l10n_mx_edi_loadfromxml/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Set EDI from attached xml',
+    'version': '15.0',
+    "author": "DGL-Latam",
+    "license": "LGPL-3",
+    'category': 'Hidden',
+    'summary': 'Load the xml as edi document (CFDI 3.3)',
+    'depends': [
+        'l10n_mx_edi',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
---
+++ l10n_mx_edi_loadfromxml/models/account_move.py
@@ -0,0 +1,33 @@
+from odoo import models, fields, exceptions, _
+
+from lxml.objectify import fromstring
+from lxml import etree
+
+import logging
+import base64
+
+_logger = logging.getLogger(__name__)
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+    
+    def load_xml_as_edi(self):
+        attachments = self.env['ir.attachment'].search([('res_id','=',self.id),('res_model','=',self._name),('name','ilike','%.xml')])
+        
+        if not attachments:
+            exceptions.ValidationError('No se encontro nigun xml adjunto')
+        for attach in attachments:
+            decoded = base64.b64decode(attach.datas)
+            root = etree.fromstring(decoded)
+            _logger.info(root.tag)
+            _logger.info(root.get('Fecha'))
+            _logger.info(root.find('.//{http://www.sat.gob.mx/cfd/3}Emisor').get('Rfc'))
+            _logger.info(root.find('.//{http://www.sat.gob.mx/TimbreFiscalDigital}TimbreFiscalDigital').get('UUID'))
+            
+            values = {
+                'move_id' : self.id,
+                'edi_format_id' : self.env.ref('l10n_mx_edi.edi_cfdi_3_3').id,
+                'attachment_id' : attach.id,
+                'state' : 'sent',
+            }
+            self.env['account.edi.document'].create(values)
