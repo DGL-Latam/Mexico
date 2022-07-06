88f6b1d6c8f9960937bf4165dfff05fcb4a04927
Istanolion <diego.rojas@digicellmx.com>
2022-07-06 16:46:35 -0500

    missing save

---
+++ avoid_reversal_naming_creditNote/models/account_move_reversal.py
@@ -0,0 +1,21 @@
+from odoo import models, fields, api
+
+class AccountMoveReversal(models.TransientModel):
+    """
+    Account move reversal wizard, it cancel an account move by reversing it.
+    """
+    _inherit = 'account.move.reversal'
+
+    def _prepare_default_reversal(self, move):
+        reverse_date = self.date if self.date_mode == 'custom' else move.date
+        return {
+            'ref': _('%(move_name)s, %(reason)s', move_name=move.name, reason=self.reason) 
+                   if self.reason
+                   else _('%s', move.name),
+            'date': reverse_date,
+            'invoice_date': move.is_invoice(include_receipts=True) and (self.date or move.date) or False,
+            'journal_id': self.journal_id.id,
+            'invoice_payment_term_id': None,
+            'invoice_user_id': move.invoice_user_id.id,
+            'auto_post': True if reverse_date > fields.Date.context_today(self) else False,
+        }
