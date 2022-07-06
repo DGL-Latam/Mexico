dd926f3e15f901a4425e23539d72edce8b463fa6
Istanolion <diego.rojas@digicellmx.com>
2022-07-06 16:44:59 -0500

    added avoid reversal naming on credit notes

---
+++ avoid_reversal_naming_creditNote/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Avoid Reversal naming when creating a credit note',
+    'version': '15.0',
+    "author": "DGL-Latam",
+    'category': 'Hidden',
+    'summary': 'Unicamente para evitar que se ponga en la razon, reversa de <nombre de la factura>',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/sale_views.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
