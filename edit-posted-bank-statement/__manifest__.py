9548f10a69549e706497120609a6977b57bf1230
Istanolion <diego.rojas@digicellmx.com>
2022-07-20 10:48:24 -0500

    added new function/module you can now edit posted bank statement allowing to add more lines to it, without needing to revert it to new

---
+++ edit-posted-bank-statement/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Edit posted bank statements',
+    'author': 'DGL-Latam',
+    'license': 'LGPL-3',
+    'category': 'Accounting/Accounting',
+    "version": "15.0",
+    'depends': [
+        'account',
+        'l10n_mx_edi',
+              
+    ],
+    'test': [],
+    'data': [   
+        'data/account_bank_statement_view.xml'
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
