c2efbca33d4dadf1f31f0fba933554b9e9a1d334
Istanolion <diego.rojas@digicellmx.com>
2022-07-22 14:30:51 -0500

    changed how credit notes have the reference

--- avoid_reversal_naming_creditNote/models/account_move_reversal.py
+++ avoid_reversal_naming_creditNote/models/account_move_reversal.py
@@ -9,10 +9,15 @@ class AccountMoveReversal(models.TransientModel):
 
     def _prepare_default_reversal(self, move):
         reverse_date = self.date if self.date_mode == 'custom' else move.date
+        reference = ""
+        if self.reason:
+            reference = _('%(reason)s', reason = self.reason)
+        elif move.ref:
+            reference = _('%(reason)s', reason = move.ref)
+        else:
+            reference = _('%(reason)s', reason = move.name)
         return {
-            'ref': _('%(move_name)s, %(reason)s', move_name=move.name, reason=self.reason) 
-                   if self.reason
-                   else _('%s', move.name),
+            'ref': reference,
             'date': reverse_date,
             'invoice_date': move.is_invoice(include_receipts=True) and (self.date or move.date) or False,
             'journal_id': self.journal_id.id,
