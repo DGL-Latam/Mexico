d17767f66c78195303f71b89a55d867a0820c6ac
Istanolion <diego.rojas@digicellmx.com>
2022-09-06 09:00:22 -0500

    fix error didnt save manifest

---
+++ at_hand_inventory_locations/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Select inventory at hand",
+    "summary": "The admin can select witch locations will be considered as on hand",
+    "version": "1.0",
+    "author": "DGL-Latam",
+    "depends" : ['stock'],
+    "data": [
+        'views/res_config_settings_views.xml',
+    ]
+}
