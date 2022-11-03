c32c66bac32ecbfc412da8c4fd5434882d1fae54
Istanolion <diego.rojas@digicellmx.com>
2022-11-03 09:18:55 -0600

    [add] inital config in manifest file

---
+++ l10n_mx_edi_nomina/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Nomina Mexico CFDI 4.0',
+    'version': '0.1',
+    "author": "DGL-Latam",
+    "license": "LGPL-3",
+    'summary': 'Modulo para poder emitir recibos de nomina en Mexico junto con su comprobante fiscal en la version 4.0',
+    'depends': [
+        'l10n_mx_edi_40',
+        'hr_payroll',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
+
