84f408c108407015ff4da2969ae7a5d716bc075c
Diego <diego.rojas@digicellmx.com>
2022-11-30 09:25:28 -0600

    [fix] init file

---
+++ l10n_mx_descarga_cfdi/models/__init__.py
@@ -0,0 +1,13 @@
+from .Autentificacion import Autentificacion
+from .DescargaMasiva import DescargaMasiva
+from .Fiel import Fiel
+from .SolicitudDescarga import SolicitudDescarga
+from .Verificacion import VerificaSolicitudDescarga
+
+__all__ = [
+    'Autentificacion',
+    'DescargaMasiva',
+    'Fiel',
+    'SolicitudDescarga',
+    'VerificaSolicitudDescarga',
+]
