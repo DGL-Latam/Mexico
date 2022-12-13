1679e1e0299c7da8e03ec8dc48df3b8c194b1aae
Diego <diego.rojas@digicellmx.com>
2022-12-13 17:42:01 -0600

    [update] document is created and saved correctly also generated all moves

--- l10n_mx_descarga_cfdi/models/FacturasSat.py
+++ l10n_mx_descarga_cfdi/models/FacturasSat.py
@@ -1,8 +1,16 @@
 from odoo import models, fields
 import base64
 import datetime
+import pytz
 import os
 import time
+import zipfile
+import io
+
+
+from lxml.objectify import fromstring
+from lxml import etree
+from dateutil import parser
 
 from .Autentificacion import Autentificacion
 from .Fiel import Fiel
@@ -51,14 +59,14 @@ class SolicitudesDescarga(models.Model):
     def _NuevaSolicitud(self,company):
         fiel = self._getFiel(company)
         token = self._getToken(company,fiel)
-        _logger.critical(token)
         solicitudDes = SolicitudDescarga(fiel)
-
+        datetime_str = '07/12/22 00:00:00'
+        datetime_str2 = '07/12/22 23:59:58'
         solicitud = solicitudDes.SolicitarDescarga(
             token,
             company.vat, 
-            datetime.datetime.today() - datetime.timedelta(days=5),
-            datetime.datetime.now(),
+            datetime.datetime.strptime(datetime_str, '%d/%m/%y %H:%M:%S'),
+            datetime.datetime.strptime(datetime_str2, '%d/%m/%y %H:%M:%S'),
             rfc_receptor = company.vat
         )
         self.env['solicitud.descarga'].sudo().create( {
@@ -66,53 +74,137 @@ class SolicitudesDescarga(models.Model):
             'estado_solicitud' : '1' if solicitud['mensaje'] == 'Solicitud Aceptada' else '0',
             'company_id' : company.id
         } )
-        _logger.critical(solicitud)
-
         
+    def _getNodes(self, cfdi_data : str):
+        def get_node(cfdi_node, attribute, namespaces):
+            if hasattr(cfdi_node, 'Complemento'):
+                node = cfdi_node.Complemento.xpath(attribute, namespaces=namespaces)
+                return node[0] if node else None
+            else:
+                return None
+        try:
+            cfdi_node = fromstring(cfdi_data)
+            emisor_node = cfdi_node.Emisor
+            receptor_node = cfdi_node.Receptor
+
+            tfd_node = get_node(
+            cfdi_node,
+            'tfd:TimbreFiscalDigital[1]',
+            {'tfd': 'http://www.sat.gob.mx/TimbreFiscalDigital'},
+            )
+        except etree.XMLSyntaxError:
+            file = io.BytesIO(cfdi_data)
+
+            for element in etree.iterparse(file):
+                if '}Emisor' in element[1].tag:
+                    emisor_node = element[1]
+                if '}Receptor' in element[1].tag:
+                    receptor_node = element[1]
+                if '}Comprobante' in element[1].tag:
+                    cfdi_node = element[1]
+                if '}TimbreFiscalDigital' in element[1].tag:
+                    tfd_node = element[1]
+        except AttributeError:
+            # Not a CFDI
+            return {}
+        return {
+            'cfdi_node' : cfdi_node,
+            'emisor_node' : emisor_node,
+            'receptor_node' : receptor_node,
+            'tfd_node' : tfd_node
+        }
+
+    def _ProcessXML(self, xml : str):
+        nodes = self._getNodes(xml)
+        if not nodes:
+            return
+        reg = self.env['facturas.sat'].sudo().search([('sat_uuid','=',nodes['tfd_node'].get('UUID'))])
+        if reg.id:
+            return
+        mexTime = pytz.timezone('America/Mexico_City')
+        f_emitido = mexTime.localize(datetime.datetime.strptime( nodes['cfdi_node'].get('Fecha') , '%Y-%m-%dT%H:%M:%S'))
+        f_timbrado = mexTime.localize(datetime.datetime.strptime( nodes['tfd_node'].get('FechaTimbrado') , '%Y-%m-%dT%H:%M:%S'))
+        fact = self.env['facturas.sat'].sudo().create({
+            'sat_uuid' : nodes['tfd_node'].get('UUID'),
+            'sat_rfc_emisor' : nodes['emisor_node'].get('Rfc', nodes['emisor_node'].get('rfc')),
+            'sat_monto' : float(nodes['cfdi_node'].get('Total')),
+            'sat_fecha_emision' :  f_emitido.astimezone(pytz.utc).replace(tzinfo=None),
+            'sat_fecha_timbrado' : f_timbrado.astimezone(pytz.utc).replace(tzinfo=None) ,
+            'sat_tipo_factura' : nodes['cfdi_node'].get('TipoDeComprobante'),
+            'company' : self.company_id.id
+        })
+        fact.SearchOdooInvoice()
+        
+    def printEstado(self):
+        for rec in self:
+            _logger.critical(rec.estado_solicitud)
+    
+    def checkzip(self):
+        for rec in self:
+            if rec.document_downloaded.id:
+                rec._ProcessZip(rec.document_downloaded.attachment_id.raw)
+                
+    def _ProcessZip(self, zipBytes):
+        z = zipfile.ZipFile(io.BytesIO(zipBytes))
+        for filename in z.infolist():
+            file = z.read(filename)
+            self._ProcessXML(file)
+        z.close()
+        
+    def attachzip(self):
+        for rec in self:
+            if rec.estado_solicitud == '3':
+                document = self.env['documents.document'].search([('name', 'ilike',rec.id_solicitud)], limit=1)
+                rec.write({'document_downloaded' :  document.id})
+    
     def _VerificarSolicitud(self):
         for rec in self:
             
             fiel = self._getFiel(self.company_id)
             token = self._getToken(self.company_id,fiel)
-            _logger.critical(rec)
-            _logger.critical(rec.id_solicitud)
             verificacion = VerificaSolicitudDescarga(fiel)
             verificacionValues = verificacion.VerificarDescarga(token=token,rfc_solicitante=rec.company_id.vat, id_solicitud=rec.id_solicitud)
             estado = int( verificacionValues['estado_solicitud'] ) 
             rec.write( {'estado_solicitud' : verificacionValues['estado_solicitud']  } ) 
-            _logger.critical(verificacionValues)
             
             if estado == 3:
                 for paquete in verificacionValues['paquetes']:
                     descarga = DescargaMasiva(fiel)
                     zip_data = descarga.DescargarPaquete(token,rec.company_id.vat,paquete)
-                    _logger.critical(zip_data)
                     if zip_data['paquete_b64'] is not None:
                         ir_attachment = self.env['ir.attachment'].sudo().create( {
                             'name' : rec.id_solicitud,
                             'type' : 'binary',
                             'raw' : base64.b64decode(zip_data['paquete_b64']),
                         })
-                        folder = self.env['documents.folder'].sudo().search([('name','ilike','internal'),('company_id','=',rec.company_id.id)])
+                        folder = self.env['documents.folder'].sudo().search([('name','ilike','SAT'),('company_id','=',rec.company_id.id)])
                         document = self.env['documents.document'].sudo().create({
                             'attachment_id' : ir_attachment.id,
                             'type' : 'binary',
                             'folder_id' : folder.id
-                        })
-                        _logger.critical(zip_data)
+                            })
+                        rec.sudo().write({'document_downloaded' : document.id})
                 
             
 class FacturasSat(models.Model):
     _name = "facturas.sat"
 
-    _description = "Tabla para guardar la relacion de las facturas encontradas en el sat mediante el Web service, y las que existen en sistema"
+    _description = "Facturas SAT"
 
-    sat_uuid = fields.Char(string="Folio Fiscal", copy=False, readonly=True)
+    sat_uuid = fields.Char(string="Folio Fiscal", copy=False, readonly=True,  required=True)
     sat_state = fields.Char(string="Estado Factura SAT")
     sat_rfc_emisor = fields.Char(string="RFC Emisor")
     sat_monto = fields.Float(string="Monto")
-    sat_fecha_emision = fields.Date(string="Fecha Emision")
-    sat_fecha_cancelacion = fields.Date(string="Fecha Cancelacion")
+    sat_fecha_emision = fields.Datetime(string="Fecha Emision")
+    sat_fecha_timbrado = fields.Datetime(string="Fecha de Timbrado")
+    sat_fecha_cancelacion = fields.Datetime(string="Fecha Cancelacion")
+    sat_tipo_factura = fields.Selection(selection = [
+        ('I', 'Ingreso'),
+        ('E', 'Egreso'),
+        ('P', 'Pago'),
+        ('N', 'Nomina'),
+        ('T', 'Traslado (Carta Porte)'),
+    ], default='1', string='Tipo de Factura')
 
     account_move_id = fields.Many2one(
         comodel_name='account.move',
@@ -120,3 +212,18 @@ class FacturasSat(models.Model):
     account_move_status = fields.Selection(string="Estado Factura Odoo", related='account_move_id.state', readonly=True)
 
     company = fields.Many2one(comodel_name="res.company",string="Empresa")
+
+    _sql_constraints = [
+        ('sat_uuid_unique',
+        'unique(sat_uuid)', 'No se deben repetir UUID'),
+    ]
+    
+    
+    def SearchOdooInvoice(self):
+        for r in self:
+            fact = r.env['account.move'].search([('company_id', '=', r.company.id), ('l10n_mx_edi_cfdi_uuid','=',r.sat_uuid)])
+            r.account_move_id = fact.id
+
+    def printinfo(self):
+        for r in self:
+            _logger.critical(f'fecha timbrado: {r.sat_fecha_timbrado}  fecha emision: {r.sat_fecha_emision}')
--- l10n_mx_edi_uuid/models/account_invoice.py
+++ l10n_mx_edi_uuid/models/account_invoice.py
@@ -1,8 +1,4 @@
 from odoo import _, api, fields, models
-from odoo.exceptions import ValidationError
-from odoo.osv.expression import (NEGATIVE_TERM_OPERATORS,
-                                 TERM_OPERATORS_NEGATION)
-TERM_OPERATORS_POSITIVE = {v: k for k, v in TERM_OPERATORS_NEGATION.items()}
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
@@ -23,9 +19,4 @@ class AccountMove(models.Model):
             move.edi_document_ids._process_documents_web_services()
             cfdi_info = move._l10n_mx_edi_decode_cfdi()
             move.l10n_mx_edi_cfdi_uuid = cfdi_info.get('uuid')
-     
-    def _searchUuid(self,operator,value):
-        if not value:
-            return [('l10n_mx_edi_cfdi_uuid',operator,value)]
-        domain_op = 'not in' if operator in NEGATIVE_TERM_OPERATORS else 'in'
-        return [('l10n_mx_edi_cfdi_uuid',operator,value)]
+    
