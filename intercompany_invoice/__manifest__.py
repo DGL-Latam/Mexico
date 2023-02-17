ac1c7eaf295fdbf5742be2a7a14eda545cdbabe9
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-02-17 11:32:29 -0600

    [update] manifest and init

---
+++ intercompany_invoice/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Intercompany Invoices',
+    'version': '15.0.0.1',
+    'author': 'DGL-Latam',
+    'description': 'Facturas intercompañias',
+    'depends': ['sale_management', 'account', 'stock', 'account_accountant', 'purchase', 'account_inter_company_rules']
+}
