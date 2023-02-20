83439397185d8b4c6816b5017918f14087dd5797
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-02-20 13:53:22 -0600

    new option on rule_type

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -5,3 +5,8 @@ _logger = logging.getLogger(__name__)
 
 class AccountMove(models.Model):
     _inherit = "account.move"
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    rule_type= fields.Selection(selection_add=[("sale_purchase_invoice_refund", "Synchronize Sales/Purchase Order and Invoices/Bills")])
