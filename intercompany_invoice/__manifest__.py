24dbfc8129d4a6c9ea4d734d2563c288679b999a
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-02-20 12:17:22 -0600

    manifest license LGPL-3

--- intercompany_invoice/__manifest__.py
+++ intercompany_invoice/__manifest__.py
@@ -3,5 +3,6 @@
     'version': '15.0.0.1',
     'author': 'DGL-Latam',
     'description': 'Facturas intercompañias',
+    'license': 'LGPL-3',
     'depends': ['sale_management', 'account', 'stock', 'account_accountant', 'purchase', 'account_inter_company_rules']
 }
