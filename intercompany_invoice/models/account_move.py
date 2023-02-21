c06598830b23cc47c8bf8f4837c5be91b9511e01
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-02-21 11:31:24 -0600

    Test

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -5,7 +5,7 @@ _logger = logging.getLogger(__name__)
 
 class AccountMove(models.Model):
     _inherit = "account.move"
-
+'''
     def _post(self, soft=True):
         records = self.env[self._name]
         records_so = self.env[self._name]
@@ -65,7 +65,7 @@ class AccountMove(models.Model):
                 'move_id': related.id,
             })
         return result
-
+'''
 
 
 
