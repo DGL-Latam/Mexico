f7ac0385f4384d1276d7d49fcf4f834e61d94428
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-02-24 14:35:44 -0600

    Test

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -5,7 +5,7 @@ _logger = logging.getLogger(__name__)
 
 class AccountMove(models.Model):
     _inherit = "account.move"
-'''
+
     def _post(self, soft=True):
         records = self.env[self._name]
         records_so = self.env[self._name]
@@ -30,9 +30,27 @@ class AccountMove(models.Model):
         result = super(AccountMove, self.with_context(disable_after_commit=True))._post(soft=soft)
         result.edi_document_ids._process_documents_web_services()
 
+        for invoice in records:
+            related = self.sudo().search([('auto_invoice_id', '=', invoice.id)])
+            if not related:
+                continue
+            filename = ('%s-%s-MX-Invoice-%s.xml' % (
+                related.journal_id.code, related.payment_reference or '', company.vat or '')).replace('/', '')
+            document = invoice._get_l10n_mx_edi_signed_edi_document()
+            attachment = document.attachment_id
+            copiedAttach = attachment.sudo().copy({
+                'res_id': related.id,
+                'company_id': related.company_id.id,
+            })
+            document.sudo().copy({
+                'move_id': related.id,
+                'attachment_id': copiedAttach.id,
+                'name': filename,
+            })
+
         
         return result
-'''
+
 
 class ResCompany(models.Model):
     _inherit = "res.company"
