3bd6164e47efb0c5ef10efbd9011b5c91a45d293
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-02-24 12:20:55 -0600

    Test

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -30,9 +30,49 @@ class AccountMove(models.Model):
         result = super(AccountMove, self.with_context(disable_after_commit=True))._post(soft=soft)
         result.edi_document_ids._process_documents_web_services()
 
-        
-
-
+        for invoice in records:
+            related = self.sudo().search([('auto_invoice_id', '=', invoice.id), ('company_id', '=', invoice.company_id.id)])
+            if not related:
+                continue
+            filename = ('%s-%s-MX-Invoice-%s.xml' % (
+                related.journal_id.code, related.payment_reference or '', company.vat or '')).replace('/', '')
+            document = invoice._get_l10n_mx_edi_signed_edi_document()
+            attachment = document.attachment_id
+            copiedAttach = attachment.sudo().copy({
+                'res_id': related.id,
+                'company_id': related.company_id.id,
+            })
+            document.sudo().copy({
+                'move_id': related.id,
+                'attachment_id': copiedAttach.id,
+                'name': filename,
+            })
+        for invoice in records_so:
+            sale = invoice.mapped('invoice_line_ids.sale_line_ids.order_id')
+            if not sale:
+                continue
+            related = self.env['purchase.order'].sudo().search([('auto_sale_order_id', '=', sale.id), ('company_id', '=', sale.company_id.id)])
+            if not related:
+                continue
+            bill = related.invoice_ids
+            if bill:
+                filename = ('%s-%s-MX-Invoice-%s.xml' % (
+                    bill.journal_id.code, bill.payment_reference or '', bill.company_id.vat or '')).replace('/', '')
+                document = invoice._get_l10n_mx_edi_signed_edi_document()
+                attachment = document.attachment_id
+                copiedAttach = attachment.sudo().copy({
+                    'res_id': bill.id,
+                    'company_id': bill.company_id.id,
+                })
+                document.sudo().copy({
+                    'move_id': bill.id,
+                    'attachment_id': copiedAttach.id,
+                    'name': filename,
+                })
+                continue
+            invoice._get_l10n_mx_edi_signed_edi_document().sudo().copy({
+                'move_id': related.id,
+            })
         return result
 
 
