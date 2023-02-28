36d10a7ea3bf5f58b460c78e4fc764c65e8b37d7
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-02-28 17:07:30 -0600

    Test

--- intercompany_invoice/models/res_company.py
+++ intercompany_invoice/models/res_company.py
@@ -6,3 +6,8 @@ class ResCompany(models.Model):
     new_rule_type = [("sale_purchase_invoice_refund", "Sincronizar órdenes de venta/compra y facturas/recibos")]
 
     rule_type = fields.Selection(selection_add=new_rule_type, string="Rule", default="not_synchronize")
+
+    @api.model
+    def _find_company_from_partner(self, partner_id):
+        company = self.sudo().search([("partner_id", "=", partner_id)], limit=1)
+        return company or False
