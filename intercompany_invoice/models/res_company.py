35eab20bf446c437d922c00d293447d8a6575f8a
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-02-28 16:33:31 -0600

    Test

--- intercompany_invoice/models/res_company.py
+++ intercompany_invoice/models/res_company.py
@@ -7,9 +7,3 @@ class ResCompany(models.Model):
     _inherit = "res.company"
 
     rule_type = fields.Selection(selection_add=list(new_rule_type), string="Rule", default="not_synchronize")
-
-
-    @api.model
-    def _find_company_from_partner(self, partner_id):
-        company =self.sudo().search(["partner_id", "=", partner_id], limit=1)
-        return company or False
--- intercompany_invoice/models/res_config_settings.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields, api
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = "res.config.settings"
-
-    rule_type = fields.Selection(related="company_id.rule_type", readonly=False)
