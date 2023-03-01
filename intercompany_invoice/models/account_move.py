86b3b6ec870c791dfd59c0d6d9ddaf643d8715b9
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-01 10:33:14 -0600

    Test

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -78,3 +78,44 @@ class AccountMove(models.Model):
             })
         
         return result
+
+
+class sale_order(models.Model):
+    _inherit = "sale.order"
+
+    def _action_confirm(self):
+        res = super(sale_order, self)._action_confirm()
+        for order in self:
+            if not order.company_id:
+                continue
+            company = self.env["res.company"]._find_company_from_partner(order.partner_id.id)
+            if company and company.rule_type in ('sale', 'sale_purchase', "sale_purchase_invoice_refund") and (not order.auto_generated):
+                order.with_user(company.intercompany_user_id).with_context(default_company_id=company.id).with_company(company).inter_company_create_purchase_order(company)
+        return res
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def _purchase_service_create(self, quantity=False):
+        line_to_purchase = set()
+        for line in self:
+            company = self.env['res.company']._find_company_from_partner(line.order_id.partner_id.id)
+            if not company or company.rule_type not in (
+            'sale', 'sale_purchase', 'sale_purchase_invoice_refund') and not line.order_id.auto_generated:
+                line_to_purchase.add(line.id)
+        line_to_purchase = self.env['sale.order.line'].browse(list(line_to_purchase))
+        return super(SaleOrderLine, line_to_purchase)._purchase_service_create(quantity=quantity)
+
+class purchase_order(models.Model):
+    _inherit = "purchase.order"
+
+    def button_approve(self, force=False):
+
+        res = super(purchase_order, self).button_approve(force=force)
+        for order in self:
+            company_rec = self.env['res.company']._find_company_from_partner(order.partner_id.id)
+            if company_rec and company_rec.rule_type in ('sale', 'sale_purchase', 'sale_purchase_invoice_refund') and (not order.auto_generated):
+                order.with_user(company_rec.intercompany_user_id).with_context(
+                    default_company_id=company_rec.id).with_company(company_rec).inter_company_create_sale_order(
+                    company_rec)
+        return res
