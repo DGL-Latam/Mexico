186e579c0f6724c0e8ce24cfb5c29d59c1d94a3f
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-02 11:00:36 -0600

    purchase_order

--- intercompany_invoice/models/purchase_order.py
+++ intercompany_invoice/models/purchase_order.py
@@ -1,23 +1,16 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models, _
-from odoo.exceptions import UserError
-
 
 class purchase_order(models.Model):
     _inherit = "purchase.order"
 
-    def _prepare_sale_order_data(self, name, partner, company, direct_delivery_address):
-        super()._prepare_sale_order_data(company)
+    def _prepare_sale_order_data(self):
 
-        def button_approve(self, force=False):
-            """ Generate inter company sales order base on conditions."""
-            res = super(purchase_order, self).button_approve(force=force)
-            for order in self:
-                # get the company from partner then trigger action of intercompany relation
-                company_rec = self.env['res.company']._find_company_from_partner(order.partner_id.id)
-                if company_rec and company_rec.rule_type in ('sale_purchase_invoice_refund') and (
-                not order.auto_generated):
-                    order.with_user(company_rec.intercompany_user_id).with_context(
-                        default_company_id=company_rec.id).with_company(company_rec).inter_company_create_sale_order(
-                        company_rec)
-            return res
+        res = super(purchase_order, self).button_approve(force=force)
+        for order in self:
+            # get the company from partner then trigger action of intercompany relation
+            company_rec = self.env['res.company']._find_company_from_partner(order.partner_id.id)
+            if company_rec and company_rec.rule_type == 'sale_purchase_invoice_refund' and (not order.auto_generated):
+                order.with_user(company_rec.intercompany_user_id).with_context(
+                    default_company_id=company_rec.id).with_company(company_rec).inter_company_create_sale_order(company_rec)
+        return res
