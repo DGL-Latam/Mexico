00adae5bbc8ea2f5bf715d00a177addf26d6eddf
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-02 13:52:51 -0600

    warehouse_id on res_config_settings

--- intercompany_invoice/models/res_config_settings.py
+++ intercompany_invoice/models/res_config_settings.py
@@ -6,3 +6,9 @@ class ResConfigSettings(models.TransientModel):
     rule_type = fields.Selection(related="company_id.rule_type", readonly=False)
     intercompany_user_id = fields.Many2one(related='company_id.intercompany_user_id', readonly=False, required=True)
     rules_company_id = fields.Many2one(related='company_id', string='Select Company', readonly=True)
+
+    warehouse_id = fields.Many2one(
+        related='company_id.warehouse_id',
+        string='Warehouse For Purchase Orders',
+        readonly=False,
+        domain=lambda self: [('company_id', '=', self.env.company.id)])
