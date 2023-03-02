e3555cbbc7f06f7aab21a3476159ae0df7a0b1fb
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-02 08:37:05 -0600

    Test

--- intercompany_invoice/models/res_config_settings.py
+++ intercompany_invoice/models/res_config_settings.py
@@ -9,13 +9,3 @@ class ResConfigSettings(models.TransientModel):
 
     auto_validation = fields.Boolean(related="company_id.auto_validation", readonly=False)
     warehouse_id = fields.Many2one(related = "company_id.warehouse_id", readonly = False, domain = lambda self: [("company_id", "=", self.env.company.id)])
-
-    @api.onchange('rule_type')
-    def onchange_rule_type(self):
-        if self.rule_type not in [("sale_purchase_invoice_refund", "Sincronizar órdenes de venta/compra y facturas/recibos")].keys():
-            self.auto_validation = False
-            self.warehouse_id = False
-        else:
-            warehouse_id = self.warehouse_id or self.env['stock.warehouse'].search(
-                [('company_id', '=', self.env.company.id)], limit=1)
-            self.warehouse_id = warehouse_id
