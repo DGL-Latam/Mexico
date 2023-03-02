6be88b7a7bb1c3fa641d7320906cd508fc40c3c5
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-02 17:40:18 -0600

    Modification on manifest

--- intercompany_invoice/__manifest__.py
+++ intercompany_invoice/__manifest__.py
@@ -6,7 +6,8 @@
     'license': 'LGPL-3',
     'depends': [
                 'account_inter_company_rules',
-                'sale_purchase_inter_company_rules'],
+                'sale_purchase_inter_company_rules',
+            'intercompany_auto_picking_reception'],
     'data': ['views/res_config_settings_sale_invoice_views.xml',
              'views/inter_company_so_invoice_view.xml']
 }
