f321dea27f962fadfe42cca1cb203e1871a59719
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-07 13:50:35 -0600

    Test

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -18,7 +18,7 @@ class AccountMove(models.Model):
 
             context = dict(self.env.context, default_company_id=company.id)
             context.pop('default_journal_id', None)
-            context.get("create_bill")
+            context["create_bill"] = True
             invoices.with_user(company.intercompany_user_id).with_context(context).with_company(company)._inter_company_create_invoices()
 
         return posted
