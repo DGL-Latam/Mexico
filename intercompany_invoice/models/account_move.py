40667af6de7f110e3e947e445bd9fb16c40a33bc
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-07 17:10:31 -0600

    Changes on purchase order

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -7,7 +7,6 @@ _logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-
     def _post(self, soft=True):
 
         invoices_map = {}
@@ -18,7 +17,7 @@ class AccountMove(models.Model):
                 invoices_map.setdefault(company, self.env['account.move'])
                 invoices_map[company] += invoice
         for company, invoices in invoices_map.items():
-            context = dict(self.env.context, default_company_id=company.id, create_bill=True)
+            context = dict(self.env.context, default_company_id=company.id)
             context.pop('default_journal_id', None)
             invoices.with_user(company.intercompany_user_id).with_context(context).with_company(company)._inter_company_create_invoices()
 
--- intercompany_invoice/models/purchase_order.py
+++ intercompany_invoice/models/purchase_order.py
@@ -15,3 +15,9 @@ class purchase_order(models.Model):
                 order.with_user(company_rec.intercompany_user_id).with_context(
                     default_company_id=company_rec.id).with_company(company_rec).inter_company_create_sale_order(company_rec)
         return res
+
+    def action_create_invoice(self):
+        confirm_from_sale = self.env["account.move"].action_post()
+        if confirm_from_sale:
+            res = super().action_create_invoice()
+        return res
