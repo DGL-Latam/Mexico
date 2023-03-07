de2d4791e630d4c579aa58cbe7e6cd3ea140070e
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-07 17:42:12 -0600

    Changes on purchase order

--- intercompany_invoice/models/purchase_order.py
+++ intercompany_invoice/models/purchase_order.py
@@ -18,6 +18,19 @@ class purchase_order(models.Model):
 
     def action_create_invoice(self):
         confirm_from_sale = self.env["account.move"].action_post()
+
+        invoices_map = {}
+        res = super()._action_create_invoice()
+
         if confirm_from_sale:
-            res = super().action_create_invoice()
-            return res
+            for invoice in res.filtered(lambda move: move.is_invoice()):
+                company = self.env['res.company']._find_company_from_partner(invoice.partner_id.id)
+                if company and company.rule_type == 'sale_purchase_invoice_refund' and not invoice.auto_generated:
+                    invoices_map.setdefault(company, self.env['account.move'])
+                    invoices_map[company] += invoice
+            for company, invoices in invoices_map.items():
+                context = dict(self.env.context, default_company_id=company.id)
+                context.pop('default_journal_id', None)
+                invoices.with_user(company.intercompany_user_id).with_context(context).with_company(
+                    company)._inter_company_create_invoices()
+        return res
