79613251377fc8aac4eee2ab4694b3176d5b912b
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-09 11:09:43 -0600

    Get rid of some lines

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -4,6 +4,7 @@ from odoo import fields, models, _
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
+
     def _post(self, soft=True):
 
         invoices_map = {}
@@ -17,8 +18,5 @@ class AccountMove(models.Model):
             context = dict(self.env.context, default_company_id=company.id, create_bill=True)
             # context.pop('default_journal_id', None)
             invoices.with_user(company.intercompany_user_id).with_context(context).with_company(company)._inter_company_create_invoices()
-
-        for invoice in self:
-            self.env["purchase.order"].action_create_invoice()
 
         return posted
