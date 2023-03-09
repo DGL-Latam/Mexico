7346c5e056b0b8ddd9b84a4e5e12f86b7cd2d48d
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-09 12:13:10 -0600

    Test on account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -19,8 +19,6 @@ class AccountMove(models.Model):
             # context.pop('default_journal_id', None)
             invoices.with_user(company.intercompany_user_id).with_context(context).with_company(company)._inter_company_create_invoices()
 
-        return posted
+        return posted, self.env["purchase.order"].action_create_invoice()
 
-        for rec in self:
 
-            return self.env["purchase.order"].action_create_invoice()
