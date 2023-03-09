489d905f7f158d777fae80ab6510a2ab883159f3
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-09 13:30:13 -0600

    Test on account_move

--- intercompany_invoice/models/purchase_order.py
+++ intercompany_invoice/models/purchase_order.py
@@ -15,7 +15,3 @@ class purchase_order(models.Model):
                 order.with_user(company_rec.intercompany_user_id).with_context(
                     default_company_id=company_rec.id).with_company(company_rec).inter_company_create_sale_order(company_rec)
         return res
-
-    def action_invoice(self):
-        if self.env["account.move"].compute_action_bill():
-            super().action_create_invoice()
