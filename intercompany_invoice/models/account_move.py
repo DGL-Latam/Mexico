20adead11b38cf06247cc5dc4075a17ec3409d19
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-14 09:04:22 -0600

    Test on account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -9,6 +9,6 @@ class AccountMove(models.Model):
         res = super().action_post()
         for rec in self:
             rec.auto_invoice_id = self.env["sale.order"].sudo().search([("name", "=", rec.id)]).id
-            rec.invoice_line_ids = self.env["sale.order"].sudo().search([("name", "=", rec.id)]).id
+            #rec.invoice_line_ids = self.env["sale.order"].sudo().search([("name", "=", rec.id)]).id
             self.env["purchase.order"].create_bill()
         return res
