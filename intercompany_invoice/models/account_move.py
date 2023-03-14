33786b7a8ff7178b92e0be6affc7e626256d29ba
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-14 08:49:48 -0600

    Test on account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -4,7 +4,7 @@ from odoo import fields, models, _
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-
+'''
     def action_post(self):
         res = super().action_post()
         for rec in self:
@@ -12,3 +12,4 @@ class AccountMove(models.Model):
             rec.invoice_line_ids = self.env["sale.order"].sudo().search([("name", "=", rec.id)]).id
             self.env["purchase.order"].create_bill()
         return res
+'''
