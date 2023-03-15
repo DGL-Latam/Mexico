7d7af5971182f2a0a1f5ad8c20438f575cb79c78
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-15 17:22:55 -0600

    Test on action_post() from account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models, _
+from datetime import datetime
+
+date = fields.Date(default=datetime.today())
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def action_post(self):
         res = super().action_post()
-        if self.move_type == "in_invoice":
-            self.super().action_post()
+        for rec in self:
+            if not self.invoice_date and self.move_type=="in_invoice":
+                rec.invoice_date = date
+                rec.super().action_post()
+        
         return res
