67c7fd5d280c30e32f374ff8959fdca0807f7940
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-15 16:55:17 -0600

    Test on action_post() from account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -4,4 +4,10 @@ from odoo import fields, models, _
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-
+    def action_post(self):
+        res = super().action_post()
+        if self.move_type == "out_invoice":
+            for rec in self:
+                if rec.move_type == "in_invoice":
+                    rec.super().action_post()
+        return res
