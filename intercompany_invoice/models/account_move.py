3d737ac217fbd70d6d4fe2e2aa267b6a6fe63e97
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-15 17:01:25 -0600

    Test on action_post() from account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -6,8 +6,6 @@ class AccountMove(models.Model):
 
     def action_post(self):
         res = super().action_post()
-        if self.move_type == "out_invoice":
-            for rec in self:
-                if rec.move_type == "in_invoice":
-                    rec.super().action_post()
+        if self.move_type == "in_invoice":
+            self.super().action_post()
         return res
