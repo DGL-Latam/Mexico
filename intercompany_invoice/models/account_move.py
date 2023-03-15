73c6d4ad5222bfa54ca6ac6fc9c849063f0b0210
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-15 13:32:40 -0600

    Test on account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -4,15 +4,8 @@ from odoo import fields, models, _
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    def action_post(self):
-        super().action_post()
+    def _post(self, soft=True):
 
-        for invoice in self:
-            if not invoice.company_id:
-                continue
-            company = self.env["res.company"]._find_company_from_partner(invoice.partner_id.id)
-            if company and company.rule_type == 'sale_purchase_invoice_refund'.format():
-                invoice.env["account.move"].with_company(company)._inter_company_create_invoices()
-
-                invoice.env["purchase.order"].action_create_invoice()
-
+        posted = super()._post(soft)
+        self.env["purchase.order"].action_create_invoice()
+        return posted
