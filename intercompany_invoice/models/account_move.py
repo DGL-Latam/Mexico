521359813336e16f009ba24c3aace4b8dc08e4e1
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-14 18:06:22 -0600

    Test

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models, _
-from odoo.exceptions import UserError
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
 
     def action_post(self):
-        res = super().action_post()
+        super().action_post()
 
         for invoice in self:
             if not invoice.company_id:
                 continue
             company = self.env["res.company"]._find_company_from_partner(invoice.partner_id.id)
-            if company and company.rule_type == 'sale_purchase'.format():
+            if company and company.rule_type == 'sale_purchase_invoice_refund'.format():
                 invoice.env["purchase.order"].action_create_invoice()
-        return res
+        
