efc6026dd18a9108f0800430fba21c948d93bbc1
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-16 12:53:44 -0600

    Test on account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -8,9 +8,9 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def action_post(self):
-        res = super().action_post()
+        super().action_post()
         invoice_id = self.env["account.move"].id
         invoice_bill = self.env["account.move"].browse(invoice_id + 1)
         invoice_bill.sudo().action_post()
-        return res
+
 
