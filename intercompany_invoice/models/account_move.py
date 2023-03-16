4a18ff081c1711eb6599ad086b9005e0d5e0e967
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-16 09:42:10 -0600

    Test on account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -9,5 +9,6 @@ class AccountMove(models.Model):
 
     def action_post(self):
         res = super().action_post()
-        self.super().action_post()
+        for rec in self:
+            rec.action_post()
         return res
