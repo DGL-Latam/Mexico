637625bdfce43ad4af34af20b0f2f3924fcd252b
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-16 15:51:39 -0600

    Test on account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -9,6 +9,11 @@ class AccountMove(models.Model):
 
     def action_post(self):
         res = super().action_post()
-        bill_id = self.env["account.move"].id + 1
-        self.env["account.move"].bill_id.action_post()
+        invoice_id = self.env["account_move"].id
+        bill_id = invoice_id + 1
+        draft_id = self.env["account_move"].browse(id)
+        for rec in draft_id:
+            if rec == bill_id:
+                rec.action_post()
+
         return res
