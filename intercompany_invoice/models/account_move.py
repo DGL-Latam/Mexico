fb5b75ea73e0c5ad2d0f055a41c343eff4bf2231
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-16 15:42:53 -0600

    Test on account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -7,7 +7,8 @@ from datetime import datetime
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-
-
-
-
+    def action_post(self):
+        res = super().action_post()
+        bill_id = self.env["account.move"].id + 1
+        self.env["account.move"].bill_id.action_post()
+        return res
