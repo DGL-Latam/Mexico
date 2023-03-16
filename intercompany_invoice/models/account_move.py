bd7dce095087ac0da5a676dd31aa6c7ca18e78d0
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-16 17:01:23 -0600

    Test on account_move and purchase_order

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -6,8 +6,6 @@ class AccountMove(models.Model):
 
     def action_post(self):
         res = super().action_post()
-        for rec in range(1):
-            bill = self.env["sale.order"].auto_purchase_order_id
-            bill.action_post_bill()
+        
 
         return res
