5c5dd3adc8e7d3638493e70e7fa13797cc2e7e9c
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-17 10:57:12 -0600

    Test on account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -10,6 +10,7 @@ class AccountMove(models.Model):
         bill = self.env["sale.order"].auto_purchase_order_id
         bill_ids = self.env["purchase.order"].invoice_ids
         invoice_ids = self.env["sale.order"].invoice_ids
+
         for rec1 in bill_ids:
             for rec2 in invoice_ids:
                 if rec1.partner_ref == rec2.name:
