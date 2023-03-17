1da001b119c94c1ca5133cc197dec25dcff1f76e
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-17 08:41:04 -0600

    Test on account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -5,13 +5,12 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     def action_post(self):
-        count = 0
+
         res = super().action_post()
         bill = self.env["sale.order"].auto_purchase_order_id
         name = self.env["sale.order"].name
         if bill.partner_ref == name:
-            if count > 0:
-                bill.action_post_bill()
-                count += 1
+
+            bill.action_post_bill()
 
         return res
