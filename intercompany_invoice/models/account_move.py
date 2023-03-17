037d9346a93af83bd0ff0babe926a8b48d743b0e
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-17 12:38:04 -0600

    Test on account_move and sale_order

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -8,7 +8,7 @@ class AccountMove(models.Model):
 
         res = super().action_post()
         invoice = self.env["sale.order"]
-        invoice_ids = invoice.invoice_ids
+        invoice_ids = fields.many2many('sale.order', 'res_sale_inv', 'sale_id', 'inv_id', 'Invoices')
 
         bill = self.env["purchase.order"]
         bill_ids = bill.invoice_ids
