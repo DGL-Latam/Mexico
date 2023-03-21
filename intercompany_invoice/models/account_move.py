1b4d07fe3ea7d20a801952a85a173bc96e85d5d8
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-21 12:57:28 -0600

    Changes on account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -10,16 +10,16 @@ class AccountMove(models.Model):
         invoice = self.env["sale.order"]
         invoice_ids = invoice.invoice_ids
         invoice_ref = invoice.client_order_ref
+        i_ids = invoice.invoice_ids.browse(invoice_ids)
 
         bill = self.env["sale.order"].auto_purchase_order_id
         bill_ids = bill.invoice_ids
         bill_ref = bill.name
 
-        for rec in self:
-            temp = rec.browse(bill_ids)
-            for rec_1 in temp:
-                if rec_1.ref == invoice_ref:
-                    temp.action_post()
+        b_ids = self.env["account.move"].browse(bill_ids)
 
-
+        for rec1 in b_ids:
+            for rec2 in i_ds:
+                if rec1.ref == rec2.name:
+                    rec1.action_post()
         return res
