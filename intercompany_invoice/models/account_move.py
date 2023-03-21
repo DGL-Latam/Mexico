90cffe3ec98f32b115e0c4672ac27a3edaac2822
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-21 17:44:15 -0600

    Changes on account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -6,12 +6,10 @@ class AccountMove(models.Model):
 
     def action_post(self):
         res = super().action_post()
-        invoices = self.search([("state", "=", "draft")])
-        invoice_from_sale = res.ref
-        for rec1 in invoices:
-            invoice_bill = self.browse(invoices.rec1)
-            if invoice_bill.name == invoice_from_sale:
-                rec1.action_post()
+        invoice_name = self.env["sale.order"].name
+        bill = self.search([("ref", "=", "invoice_name")])
+        bill.action_post()
+
 
 
         return res
