f7c939fad4cb91f12c0c7a125aeae9d3b6b858cc
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-23 13:18:42 -0600

    Modification on account_move

--- intercompany_invoice/models/account_move.py
+++ intercompany_invoice/models/account_move.py
@@ -14,7 +14,7 @@ class AccountMove(models.Model):
         invoices_posted = self.env["account.move"].search([("state", "=", "posted")], limit=1)
         invoice_id = invoices_posted.id
 
-        bill_id = self.env["account.move"].search([("state", "=", "draft")])
+        bill_id = self.env["account.move"].search([("state", "=", "draft"), ("id", ">", invoice_id)])
 
         for rec1 in invoices_posted:
             _logger.info("from invoice")
@@ -28,4 +28,14 @@ class AccountMove(models.Model):
                 if rec1.invoice_origin == rec2.ref:
                     rec2.invoice_date = datetime.today()
                     rec2.action_post()
+        return res
+
+    def _check_duplicate_supplier_reference(self):
+        for order in self:
+            if not order.company_id:
+                continue
+            company = self.env["res.company"]._find_company_from_partner(order.partner_id.id)
+            if company and company.rule_type == 'sale_purchase_invoice_refund':
+                continue
+            res = super()._check_duplicate_supplier_reference()
         return res
