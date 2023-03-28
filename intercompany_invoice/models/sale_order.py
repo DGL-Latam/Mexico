59e1d5cfc22e5f037157fc7e5f15251299837372
Isaac Cordova <isaac.cordova@digicellmx.com>
2023-03-28 17:28:45 -0600

    Changes on sale_order

--- intercompany_invoice/models/sale_order.py
+++ intercompany_invoice/models/sale_order.py
@@ -25,7 +25,7 @@ class sale_order(models.Model):
     def _create_invoices(self, grouped=False, final=False, date=None):
 
         name_so = self.env["sale.order"].name
-        from_stock_picking_id = self.picking_ids.group_id
+        from_stock_picking_id = self.picking_ids
 
         _logger.info(name_so)
         _logger.info(from_stock_picking_id)
