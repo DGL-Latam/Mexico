6d8556f37cc1d665edc8fcbe8bb9aad3c0b7fbfc
yairalvarez <yair.alvarez@digicellmx.com>
2023-04-28 10:07:29 -0600

    prueba de funcionamiento de modulo

--- l10n_mx_edi_payment_split/models/account_edi_format.py
+++ l10n_mx_edi_payment_split/models/account_edi_format.py
@@ -23,7 +23,7 @@ from json.decoder import JSONDecodeError
 _logger = logging.getLogger(__name__)
 
 
-EQUIVALENCIADR_PRECISION_DIGITS = 11
+EQUIVALENCIADR_PRECISION_DIGITS = 10
 class AccountEdiFormat(models.Model):
     _inherit = 'account.edi.format'
     
@@ -192,13 +192,13 @@ class AccountEdiFormat(models.Model):
         # CFDI 4.0: rounding needs to be done after all DRs are added
         # We round up for the currency rate and down for the tax values because we lost a lot of time to find out
         # that Finkok only accepts it this way.  The other PACs accept either way and are reasonable.
-        for v in total_taxes_paid.values():
-            v['base_value'] = float_round(v['base_value'], move.currency_id.decimal_places, rounding_method='DOWN')
-            v['tax_value'] = float_round(v['tax_value'], move.currency_id.decimal_places, rounding_method='DOWN')
-            v['base_value_mxn'] = float_round(v['base_value'] / rate_payment_curr_mxn_40, mxn_currency.decimal_places)
-            v['tax_value_mxn'] = float_round(v['tax_value'] / rate_payment_curr_mxn_40, mxn_currency.decimal_places)
+#        for v in total_taxes_paid.values():
+#            v['base_value'] = float_round(v['base_value'], move.currency_id.decimal_places, rounding_method='DOWN')
+#            v['tax_value'] = float_round(v['tax_value'], move.currency_id.decimal_places, rounding_method='DOWN')
+#            v['base_value_mxn'] = float_round(v['base_value'] / rate_payment_curr_mxn_40, mxn_currency.decimal_places)
+#            v['tax_value_mxn'] = float_round(v['tax_value'] / rate_payment_curr_mxn_40, mxn_currency.decimal_places)
         for v in total_taxes_withheld.values():
-            v['amount_curr'] = float_round(v['amount_curr'], move.currency_id.decimal_places, rounding_method='DOWN')
+#            v['amount_curr'] = float_round(v['amount_curr'], move.currency_id.decimal_places, rounding_method='DOWN')
             v['amount_mxn'] = float_round(v['amount_curr'] / rate_payment_curr_mxn_40, mxn_currency.decimal_places)
 
         cfdi_values = {
