6c3b86bbce4162dddbab096584915e528c2f7e70
Diego-Gar-DGL <diego.rojas@digicellmx.com>
2023-08-08 16:50:55 -0600

    Fixes

--- l10n_mx_edi_liverpool_detallista/__manifest__.py
+++ l10n_mx_edi_liverpool_detallista/__manifest__.py
@@ -2,13 +2,13 @@
     'name': 'Complemento detallista Liverpool',
     'author': 'DGL-Latam',
     'license': 'LGPL-3',
-    "version": "0.0.1",
+    "version": "0.0.2",
     'depends': [
         'l10n_mx_edi_40',        
     ],
     'data': [   
         'data/complento_detallista.xml',
-        'views/account_model.xml'
+        'views/account_move.xml'
     ],
     'installable': True,
 }
--- l10n_mx_edi_liverpool_detallista/models/account_move.py
+++ l10n_mx_edi_liverpool_detallista/models/account_move.py
@@ -10,19 +10,19 @@ class AccountMove(models.Model):
     delivery_reference_date = fields.Date(string="Fecha contra-recibo")
 
     department_name = fields.Char(string="Contacto de compras")
-    special_service_type = fields.Selection([{
-        'AJ' : 'Ajustes',
-        'CAC' : 'Descuento en efectivo',
-        'COD' : 'Efectivo a la entrega',
-        'EAB' : 'Descuento por pronto pago',
-        'FC' : 'Costes del flete',
-        'FI' : 'Costes Financieros',
-        'HD' : 'Manipulado',
-        'QD' : 'Descuento por cantidad',
-        'AA' : 'Abono por publicidad',
-        'ADS' : 'Pedido de un pallet completo',
-        'ADT' : 'Recogida'
-    }], string="Tipo de Servicio Especial", default="AA")
-    percentage_special_service = fields.float(string="Cantidad o Porcentaje")
+    special_service_type = fields.Selection([
+        ('AJ' , 'Ajustes'),
+        ('CAC' , 'Descuento en efectivo'),
+        ('COD' , 'Efectivo a la entrega'),
+        ('EAB' , 'Descuento por pronto pago'),
+        ('FC' , 'Costes del flete'),
+        ('FI' , 'Costes Financieros'),
+        ('HD' , 'Manipulado'),
+        ('QD' , 'Descuento por cantidad'),
+        ('AA' , 'Abono por publicidad'),
+        ('ADS', 'Pedido de un pallet completo'),
+        ('ADT' , 'Recogida')
+    ], string="Tipo de Servicio Especial", default="AA")
+    percentage_special_service = fields.Float(string="Cantidad o Porcentaje")
 
     
