f5056c97f0de8144517af8c121b107e365d0676f
Gael Ramos <esgaelramos@gmail.com>
2024-06-17 09:41:00 -0600

    fix(mercado_libre) change labels of fields into res_config_settings

--- mercado_libre/models/res_config_setting.py
+++ mercado_libre/models/res_config_setting.py
@@ -11,8 +11,8 @@ class ResConfigSettings(models.TransientModel):
     ml_app_secret = fields.Char(string="App secret", help="Secret key, se puede checar en la configuracion de la app en la pagina de developers de ML", related="company_id.ml_app_secret", readonly=False)
     ml_access_token = fields.Char(string="Access Token Mercado", help="Token para poder acceder a los servicios del api", related="company_id.ml_access_token", readonly=False)
     ml_refresh_token = fields.Char(string="Refresh Token Mercado", help="Token para refrescar nuestro token de acceso", related="company_id.ml_refresh_token", readonly=False)
-    ml_responsible_deliveries = fields.Char(string="Correos Responsables entrega", related="company_id.ml_responsible_deliveries", readonly=False)
-    ml_responsible_products = fields.Char(string="Correos Responsables entrega", related="company_id.ml_responsible_products", readonly=False)
+    ml_responsible_deliveries = fields.Char(string="Correos Responsables Entregas", related="company_id.ml_responsible_deliveries", readonly=False)
+    ml_responsible_products = fields.Char(string="Correos Responsables Productos", related="company_id.ml_responsible_products", readonly=False)
 
 
     def but_refresh_code(self):
